9b9a14c85387f9f7c52d5ba9aafc6f3ccd97e83a
skyflyhighly <stepnflors@gmail.com>
2022-10-27 19:48:08 +0300

    added: Home page

---
+++ src/components/CardGroup.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    class="card-item"
+    v-for="(card, index) in props.group?.cards"
+    :key="index"
+    :style="{
+      top: generateCardOffset(index),
+    }"
+  >
+    <card :card="card"></card>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Card from '../components/Card.vue'
+
+interface Props {
+  group: {
+    type: string
+    cards: {
+      type: string
+    }[]
+  }
+}
+
+const props = defineProps<Props>()
+
+function generateCardOffset(index: number): string {
+  return `${10 * index}px`
+}
+</script>
+
+<style scoped>
+:host {
+  display: block;
+  position: relative;
+}
+
+.card-item {
+  position: absolute;
+  width: 100%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,40 @@
 <template>
-  <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Ionic Vite - Home</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content class="ion-padding">
-      <h1>Home</h1>
-      <ion-button router-link="/about">About</ion-button>
-    </ion-content>
-  </ion-page>
+  <ion-content class="ion-padding">
+    <div class="header">
+      <ion-title size="large" class="float-left">Wallet</ion-title>
+
+      <!-- This is a placeholder element to help with the transition -->
+      <ion-button class="modal-close float-left" fill="clear">Done</ion-button>
+
+      <ion-icon
+        name="add-circle"
+        slot="icon-only"
+        class="float-right add-to-wallet trans"
+      >
+      </ion-icon>
+
+      <!-- This is a placeholder element to help with the transition -->
+      <ion-icon
+        name="ellipsis-horizontal-circle-sharp"
+        class="modal-more float-right"
+      ></ion-icon>
+
+      <div class="float-clear"></div>
+    </div>
+
+    <div class="card-groups">
+      <div
+        class="card-group trans-slow ion-activatable"
+        v-for="(cardGroup, index) in cardGroups"
+        :key="index"
+        :style="{
+          top: generateCardOffset(cardGroup, index),
+        }"
+      >
+        <card-group :group="cardGroup"> </card-group>
+      </div>
+    </div>
+  </ion-content>
 </template>
 
 <script lang="ts" setup>
@@ -21,6 +46,104 @@ import {
   IonTitle,
   IonToolbar,
 } from '@ionic/vue'
+
+import CardGroup from '../components/CardGroup.vue'
+
+interface ICardGroup {
+  cards: {
+    type: string
+  }[]
+  type: string
+}
+
+const cardGroups = [
+  {
+    type: 'apple-cash',
+    cards: [{ type: 'apple-cash' }],
+  },
+  {
+    type: 'debit',
+    cards: [{ type: 'debit' }],
+  },
+  {
+    type: 'pass',
+    cards: [{ type: 'pass' }, { type: 'pass' }],
+  },
+  {
+    type: 'generic',
+    cards: [{ type: 'generic' }, { type: 'generic' }, { type: 'generic' }],
+  },
+]
+
+function generateCardOffset(cardGroup: ICardGroup, index: number) {
+  const isDebitOrCash =
+    cardGroup.type === 'apple-cash' || cardGroup.type === 'debit'
+  const offset = isDebitOrCash ? 0 : 230
+
+  return `${45 * index + offset}px`
+}
 </script>
 
-<style></style>
+<style scoped>
+ion-content {
+  --padding-top: calc(50px + var(--ion-safe-area-top));
+}
+
+.header {
+  position: relative;
+}
+
+.header .modal-close {
+  pointer-events: none;
+  position: absolute;
+  left: 0;
+  transform-origin: top left;
+  opacity: 0;
+  font-weight: 600;
+  margin: 0;
+  --color: white;
+}
+
+.header .modal-more {
+  pointer-events: none;
+  position: absolute;
+  right: 0;
+  transform-origin: top right;
+  opacity: 0;
+  font-size: 28px;
+  margin: 0;
+}
+
+ion-title {
+  position: relative;
+  height: auto;
+  padding: 0;
+  width: 150px;
+  min-width: auto;
+  transform-origin: top left;
+}
+
+.add-to-wallet {
+  font-size: 32px;
+  margin: 8px 0;
+  transform-origin: top right;
+}
+
+.add-to-wallet:active {
+  opacity: 0.7;
+}
+
+.card-groups,
+.pass-groups {
+  position: relative;
+  margin-top: 10px;
+  z-index: 1;
+}
+
+.card-group {
+  width: 100%;
+  transform-origin: top;
+  position: relative;
+  display: block;
+}
+</style>
